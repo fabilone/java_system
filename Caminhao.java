aedc39c99e62189bd30a136a607ae9f30e5b2011
Fabilone Santos da Silva <fabilonesantos@hotmail.com>
2020-08-31 08:41:11 -0300

    Class caminhão config

--- Caminhao.java
+++ Caminhao.java
@@ -3,4 +3,17 @@ public class Caminhao extends Veiculo{
     private int comprimento;
     private int carga;
     private String carroceria;
+    private String[] modelo;
+
+    public Caminhao(){
+        super();
+        this.comprimento = 0;
+        this.carga = 0;
+        this.carroceria = "";
+        this.modelo = new String[] {"VUC", "3/4", "Toco", "Truck", "Bitruck", "Cavalo mecanico 4x2"};
+    }
+
+    public void printModelo(){
+        for(int i = 0; i < 6; ++i) System.out.println(this.modelo[i]);
+    }
 }
--- MainClass.java
+++ MainClass.java
@@ -3,8 +3,10 @@ public class MainClass{
     public static void main(String[] args ){
     Carro carro = new Carro();
     Veiculo moto = new Veiculo();
+    Caminhao caminhao = new Caminhao();
     carro.exibirInfo();
     moto.exibirInfo();
+    caminhao.printModelo();
   }
   
 }
