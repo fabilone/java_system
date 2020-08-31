684d9215feddb4906259e7b0adb5e8a329939224
Fabilone Santos da Silva <fabilonesantos@hotmail.com>
2020-08-31 08:08:21 -0300

    Create Class

---
+++ Caminhao.java
@@ -0,0 +1,6 @@
+public class Caminhao extends Veiculo{
+
+    private int comprimento;
+    private int carga;
+    private String carroceria;
+}
---
+++ Moto.java
@@ -0,0 +1,8 @@
+public class Moto extends Veiculo{
+
+    private int quantassentos;
+    private int quantpneus;
+    private int cilindradas;
+
+    
+}
