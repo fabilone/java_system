fb5b1dc0723212d4420a6e60d56c7a4fcfc149ba
Fabilone Santos da Silva <fabilonesantos@hotmail.com>
2020-08-31 13:37:36 -0300

    Class main update

--- MainClass.java
+++ MainClass.java
@@ -1,16 +1,24 @@
+import java.util.Random;
+
 public class MainClass{
     
     public static void main(String[] args ){
 
-    Carro carro = new Carro();
-    Veiculo moto = new Veiculo();
-    Moto moto2 = new Moto();
-    Caminhao caminhao = new Caminhao();
+      String[] tipoCarga = new String[] {"VUC", "3/4", "Toco", "Truck", "Bitruck", "Cavalo mecanico 4x2"};
+      String[] marcaVeiculo = new String[] {"Ford", "Honda", "Peugeot", "Volkswagen"}; 
 
-    carro.exibirInfo();
-    moto.exibirInfo();
-    moto2.exibirInfo();
-    caminhao.exibirInfo();
-  }
+      Carro carro = new Carro();
+      Veiculo moto = new Veiculo();
+      Moto moto2 = new Moto();
+      Caminhao caminhao = new Caminhao();
+
+      carro.exibirInfo();
+      moto.exibirInfo();
+      moto2.exibirInfo();
+      caminhao.exibirInfo();
+
+      for(int i = 0; i < 6; i++) System.out.println(tipoCarga[i]);
+      for(int i = 0; i < 4; i++) System.out.println(marcaVeiculo[i]);
+    }
   
 }
